13ce17be66bc9009fc59c9c6eb7f80eb4e60ed1d
unknown <amalek.devo@gmail.com>
2023-05-14 11:36:04 +0200

    create and connect to server

---
+++ chat_backend/index.js
@@ -0,0 +1,43 @@
+/* variables */
+
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+
+
+/* express server */
+
+const app = express();
+app.use(cors()); /* middleware */
+
+const httpAppServer = http.createServer(app);
+const port = process.env.PORT ?? 8000;
+httpAppServer.listen(
+    port,
+    () => {
+        console.log(`Server is running on ${port}`)
+    }
+);
+
+app.get('/', (req, res) => {
+    res.send({
+        status: 200
+    })
+});
+
+
+/* socket.io server */
+
+const { Server } = require('socket.io');
+const io = new Server(
+    httpAppServer,
+    {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+        }
+    }
+);
+io.on('connection', socket => {
+    console.log(`User ${socket.id} has connected.`)
+});
