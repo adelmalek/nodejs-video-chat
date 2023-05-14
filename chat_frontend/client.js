d41efbf745d0bac8a91e5f0cae6fa401f7ca27a3
unknown <amalek.devo@gmail.com>
2023-05-14 13:37:44 +0200

    getUserMedia and enumerateDevices

--- chat_frontend/client.js
+++ chat_frontend/client.js
@@ -1,1 +1,31 @@
 let socket = io("http://localhost:8000");
+
+
+/* stream */
+
+navigator
+    .mediaDevices
+    .getUserMedia({
+        audio: true,
+        video: true
+    })
+    .then(stream => {
+        const video = document.createElement('video');
+        video.setAttribute('autoplay', 'true');
+        video.setAttribute('playsinline', 'true');
+        video.setAttribute('controls', 'true');
+        video.srcObject = stream;
+        document.querySelector('.js-container').appendChild(video);
+    })
+    .catch(error => console.log(error.name + ": " + error.message));
+
+
+/* devices */
+
+navigator
+    .mediaDevices
+    .enumerateDevices()
+    .then(devices => devices.map(device => {
+        console.log(device.kind + ": " + device.label + " id = " + device.devideId)
+    }))
+    .catch(error => console.log(error.name + ": " + error.message));
