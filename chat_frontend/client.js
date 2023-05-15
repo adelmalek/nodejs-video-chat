d296b078fec32092dd4717a7f0cd829d3c5c2a18
unknown <amalek.devo@gmail.com>
2023-05-15 10:59:11 +0200

    start and stop stream

--- chat_frontend/client.js
+++ chat_frontend/client.js
@@ -1,23 +1,18 @@
+const SELECT_DEVICE = document.querySelector(".js-input-devices");
+const START_BTN = document.querySelector(".js-start-stream");
+const STOP_BTN = document.querySelector(".js-stop-stream");
+
 let socket = io("http://localhost:8000");
+let video = null;
 
 
-/* stream */
+/* stop stream */
 
-navigator
-    .mediaDevices
-    .getUserMedia({
-        audio: true,
-        video: true
-    })
-    .then(stream => {
-        const video = document.createElement('video');
-        video.setAttribute('autoplay', 'true');
-        video.setAttribute('playsinline', 'true');
-        video.setAttribute('controls', 'true');
-        video.srcObject = stream;
-        document.querySelector('.js-container').appendChild(video);
-    })
-    .catch(error => console.log(error.name + ": " + error.message));
+STOP_BTN.addEventListener('click', stopStream);
+
+function stopStream() {
+    document.querySelector(".js-container").innerHTML = "";
+};
 
 
 /* devices */
@@ -50,3 +45,44 @@ navigator
 function changeMicrophone(event) {
     console.log(event.target.value)
 };
+
+
+/* start stream */
+
+START_BTN.addEventListener('click', () => {
+    const microphoneId = SELECT_DEVICE.value;
+    startStream(microphoneId);
+});
+
+function startStream(audioDeviceId) {
+    navigator
+        .mediaDevices
+        .getUserMedia({
+            audio: {
+                deviceId: audioDeviceId
+            },
+            video: true
+        })
+        .then(stream => {
+            stopStream();
+            video = document.createElement('video');
+            video.setAttribute('autoplay', 'true');
+            video.setAttribute('playsinline', 'true');
+            video.setAttribute('controls', 'true');
+            video.srcObject = stream;
+            document.querySelector('.js-container').appendChild(video);
+
+            window.stream = stream;
+        })
+        .catch(error => console.log(error.name + ": " + error.message));
+};
+
+
+
+
+
+
+
+
+
+
